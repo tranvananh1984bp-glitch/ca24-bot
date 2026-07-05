81c58ca214bec1c6333cc0710dbc07e7808ee106
Van Anh <tranvananh1984bp@gmail.com>
2026-07-05 12:39:59 +0700

    CA24 bot init

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("CA24 BOT ĐANG CHẠY OK");
+});
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
